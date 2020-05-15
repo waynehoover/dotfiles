4cb5bbfc8d5b974bfb885fe1e7628a40bed093c3
Wayne Hoover <w@waynehoover.com>
2020-05-15 13:49:18 -1000

    add Brewfile delete some things i dont use

--- hammerspoon/init.lua
+++
@@ -1,4 +0,0 @@
--- Make sure to install http://www.hammerspoon.org/Spoons/WindowHalfsAndThirds.html spoon
-hs.window.animationDuration = 0
-hs.loadSpoon('WindowHalfsAndThirds')
-spoon.WindowHalfsAndThirds:bindHotkeys(spoon.WindowHalfsAndThirds.defaultHotkeys)
