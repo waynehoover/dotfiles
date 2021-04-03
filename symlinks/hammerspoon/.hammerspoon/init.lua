1929972626cd100478a778a239fe3bc848daa099
Wayne Hoover <w@waynehoover.com>
2021-04-03 12:30:03 -0600

    add hammerspoon init

--- symlinks/hammerspoon/.hammerspoon/init.lua
+++ symlinks/hammerspoon/.hammerspoon/init.lua
@@ -1,8 +1,25 @@
+local log = hs.logger.new('hammerspoon','debug')
+log.d('here')
+
+m = hs.hotkey.modal.new({}, nil)
+
+local pressed  = function() m:enter() end
+local released = function() m:exit() end
+
+local launch = function(app)
+  hs.application.launchOrFocus(app)
+end
+
+hs.hotkey.bind({}, 'f19', pressed, released)
+
+m:bind({}, '.', function() launch('Visual Studio Code'); end)
+m:bind({}, 'i', function() launch('iTerm'); end)
+m:bind({}, 'o', function() launch('Mimestream'); end)
+m:bind({}, 'e', function() launch('Brave Browser'); end)
+m:bind({}, 'a', function() launch('Slack'); end)
+
 -- If in vscode send delete line key code, otherwise send below.
 -- SEND_STRING(SS_LCTRL("d")SS_LSFT(SS_LCTRL("a")SS_TAP(X_LEFT))SS_TAP(X_BSPC));
---local log = hs.logger.new('hammerspoon','debug')
---log.d('here')
-
 hs.hotkey.bind({'alt'}, 's', function()
   local front = hs.application.frontmostApplication()
   if front and front:name() == "Code" then
@@ -12,6 +29,15 @@ hs.hotkey.bind({'alt'}, 's', function()
     hs.eventtap.event.newKeyEvent('k', false):post()
     hs.eventtap.event.newKeyEvent(hs.keycodes.map.ctrl, false):post()
     hs.eventtap.event.newKeyEvent(hs.keycodes.map.shift, false):post()
+  elseif front and front:name() == "iTerm2" then
+    hs.eventtap.event.newKeyEvent(hs.keycodes.map.ctrl, true):post()
+    hs.eventtap.event.newKeyEvent('a', true):post()
+    hs.eventtap.event.newKeyEvent('a', false):post()
+    hs.eventtap.event.newKeyEvent(hs.keycodes.map.ctrl, false):post()
+    hs.eventtap.event.newKeyEvent(hs.keycodes.map.ctrl, true):post()
+    hs.eventtap.event.newKeyEvent('k', true):post()
+    hs.eventtap.event.newKeyEvent('k', false):post()
+    hs.eventtap.event.newKeyEvent(hs.keycodes.map.ctrl, false):post()
   else
     hs.eventtap.event.newKeyEvent(hs.keycodes.map.cmd, true):post()
     hs.eventtap.event.newKeyEvent('Right', true):post()
@@ -28,4 +54,3 @@ hs.hotkey.bind({'alt'}, 's', function()
     hs.eventtap.event.newKeyEvent(hs.keycodes.map.delete, false):post()
   end
 end)
-
